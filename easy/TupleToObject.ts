02ca5b45c158ba52196d56cf02c18854e7be6905
Sinkyo <yhko1988@gmail.com>
2024-04-17 19:50:36 +0900

    feat : 주석으로 IdxOfTuple 으로 설명

--- easy/TupleToObject.ts
+++ easy/TupleToObject.ts
@@ -1,3 +1,5 @@
+// const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
 type IdxOfTuple<Tuple extends readonly string[]> = Tuple extends readonly [
   unknown,
   ...infer Tail
@@ -11,6 +13,11 @@ type TupleToObject<Tuple extends readonly string[]> = {
   [key in Tuple[number]]: key;
 };
 
+type Idx = IdxOfTuple<["model 3", "model X", "model Y"]>;
+type Idx2<Arr extends readonly any[]> = Arr["length"] | "hahah";
+
+const arr: Idx2<["s"]> = "hahah";
+
 type TupleToNarrowedObject<
   Tuple extends readonly string[],
   Idx extends IdxOfTuple<Tuple>
@@ -18,9 +25,19 @@ type TupleToNarrowedObject<
   [key in Tuple[Idx]]: key;
 };
 
-const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+/**
+ * IdxOfTuple<[0,0,0]>
+  = 2 | IdxOfTuple<[0,0]>
+  = 2 | 1 | IdxOfTuple<[0]>
+  = 2 | 1 | 0 | IdxOfTuple<[]> --> never가 리턴됨
 
-type result = TupleToNarrowedObject<typeof tuple, 0 | 2>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+  ==> 최종 리턴 값 = 2 | 1 | 0
+
+ * 
+ */
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+type result = TupleToNarrowedObject<typeof tuple, 14>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
 type result2 = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 
 const cool: result = {
