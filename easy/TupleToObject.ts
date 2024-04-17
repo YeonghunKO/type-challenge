69a104e74a784859dec2840876b1ee64d1113e8e
Sinkyo <yhko1988@gmail.com>
2024-04-17 17:57:59 +0900

    feat : tuple의 길이보다 큰 숫자는 에러를 내뿜음

--- easy/TupleToObject.ts
+++ easy/TupleToObject.ts
@@ -1,14 +1,31 @@
+type IdxOfTuple<Tuple extends readonly string[]> = Tuple extends readonly [
+  unknown,
+  ...infer Tail
+]
+  ? Tail extends string[]
+    ? Tail["length"] | IdxOfTuple<Tail>
+    : never
+  : never;
+
 type TupleToObject<Tuple extends readonly string[]> = {
   [key in Tuple[number]]: key;
 };
 
+type TupleToNarrowedObject<
+  Tuple extends readonly string[],
+  Idx extends IdxOfTuple<Tuple>
+> = {
+  [key in Tuple[Idx]]: key;
+};
+
 const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 
-type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+type result = TupleToNarrowedObject<typeof tuple, 0 | 2>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+type result2 = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
 
 const cool: result = {
-  "model 3": "model 3",
+  //   "model 3": "model 3",
   "model X": "model X",
-  "model Y": "model Y",
+  //   "model Y": "model Y",
   tesla: "tesla",
 };
