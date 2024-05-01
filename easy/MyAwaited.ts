38f93e5bf079a27fafbbac885fd68447faaa022f
Sinkyo <yhko1988@gmail.com>
2024-05-01 22:30:50 +0900

    feat : 완성!!
    - infer에 익숙해져가는중

---
+++ easy/MyAwaited.ts
@@ -0,0 +1,13 @@
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any>
+    ? MyAwaited<U>
+    : U
+  : never;
+
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type Z1 = Promise<Promise<Promise<string | boolean>>>;
+type T = { then: (onfulfilled: (arg: number) => any) => any };
+
+type Res = MyAwaited<T>;
