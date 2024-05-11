ce1f4aa1eb10207840d25829e4040698c5ba12b7
Sinkyo <yhko1988@gmail.com>
2024-05-11 16:45:25 +0900

    feat : 완성

---
+++ easy/MyOmit.ts
@@ -0,0 +1,5 @@
+type MyOmit<T extends Object, K extends keyof T> = {
+  [Key in keyof T as Key extends K ? never : Key]: T[Key];
+};
+
+type MyOmit2<T, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
