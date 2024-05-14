04619f425b760575b2e2b8a20e129d553e3763b0
Sinkyo <yhko1988@gmail.com>
2024-05-14 21:57:33 +0900

    refactor : iife적용

--- easy/MyOmit.ts
+++ easy/MyOmit.ts
@@ -1,5 +1,7 @@
-type MyOmit<T extends Object, K extends keyof T> = {
-  [Key in keyof T as Key extends K ? never : Key]: T[Key];
-};
+(() => {
+  type MyOmit<T extends Object, K extends keyof T> = {
+    [Key in keyof T as Key extends K ? never : Key]: T[Key];
+  };
 
-type MyOmit2<T, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
+  type MyOmit2<T, K extends keyof T> = { [P in Exclude<keyof T, K>]: T[P] };
+})();
