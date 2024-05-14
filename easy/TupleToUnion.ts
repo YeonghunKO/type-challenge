1ad2b7b4bab8000e2b4f73e12c84fed5ed94a52c
Sinkyo <yhko1988@gmail.com>
2024-05-14 21:58:34 +0900

    doc : 주석으로 설명

--- easy/TupleToUnion.ts
+++ easy/TupleToUnion.ts
@@ -1,5 +1,7 @@
 (() => {
   const arr = [123, "456", true];
+
+  // item안에 있는 원소의 타입이 어떤게 될지 모르니 모든 타입을 union으로 통합한것임
   type TupleToUnion<T extends any[]> = T extends Array<infer ITEM>
     ? ITEM // T[number]
     : never;
