7379e8b0929d99c52a1ffd11b68b411de47e7bb5
Sinkyo <yhko1988@gmail.com>
2024-05-15 21:55:55 +0900

    feat : init

---
+++ medium/Chainable.ts
@@ -0,0 +1,15 @@
+(() => {
+  const result1 = a
+    .option("foo", 123)
+    .option("bar", { value: "Hello World" })
+    .option("name", "type-challenges")
+    .get();
+
+  type Expected1 = {
+    foo: number;
+    bar: {
+      value: string;
+    };
+    name: string;
+  };
+})();
---
+++ medium/index.d.ts
@@ -0,0 +1,5 @@
+type Chainable = {
+  option(key: string, value: any): any;
+  get(): any;
+};
+declare const a: Chainable;
