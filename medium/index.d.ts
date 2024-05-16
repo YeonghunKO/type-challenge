f8c5d1e8e058d65743b1a183de6e9d0b797d94c9
Sinkyo <yhko1988@gmail.com>
2024-05-16 20:56:55 +0900

    feat : done

--- medium/index.d.ts
+++ medium/index.d.ts
@@ -1,5 +1,9 @@
-type Chainable = {
-  option(key: string, value: any): any;
-  get(): any;
+type Chainable<R = {}> = {
+  option<K extends keyof any, V>(
+    key: K extends keyof R ? never : K,
+    value: V
+  ): Chainable<Omit<R, K> & Record<K, V>>; // Omit없으면 같은 key가 pass되었을 경우 never가 뜸
+  get(): R;
 };
+
 declare const a: Chainable;
