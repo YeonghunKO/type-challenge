8b1c1de7b93146696bf45907b79bc1fcf9f1f44b
Sinkyo <yhko1988@gmail.com>
2024-05-23 21:02:31 +0900

    feat : union type distribution 많이 쓰임

---
+++ medium/Permutation.ts
@@ -0,0 +1,9 @@
+() => {
+  type Permutation<T, K = T> = [T] extends [never]
+    ? []
+    : K extends K
+    ? [K, ...Permutation<Exclude<T, K>>]
+    : never;
+
+  type Permuted = Permutation<"a" | "b">; // ['a', 'b'] | ['b' | 'a']
+};
