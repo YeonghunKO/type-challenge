9f886f645ea651f350bbe9ed5442999e859d7f13
Sinkyo <yhko1988@gmail.com>
2024-05-24 23:22:36 +0900

    feat : init

---
+++ medium/LengthString.ts
@@ -0,0 +1,10 @@
+() => {
+  type LengthOfString<S extends string> = any;
+
+  type cases = [
+    Expect<Equal<LengthOfString<"">, 0>>,
+    Expect<Equal<LengthOfString<"kumiko">, 6>>,
+    Expect<Equal<LengthOfString<"reina">, 5>>,
+    Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
+  ];
+};
