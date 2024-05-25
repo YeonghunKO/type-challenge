6a0469eb25deca9e9880aa2caf0d463ef8cabb4b
Sinkyo <yhko1988@gmail.com>
2024-05-25 21:49:07 +0900

    feat : done

--- medium/LengthString.ts
+++ medium/LengthString.ts
@@ -1,10 +1,13 @@
 () => {
-  type LengthOfString<S extends string> = any;
+  type LengthOfString<
+    S extends string,
+    A extends string[] = []
+  > = S extends `${infer First}${infer Second}`
+    ? LengthOfString<Second, [First, ...A]>
+    : A["length"]; // A라고해보면 어떻게 A에 쌓이는지 보임.
 
-  type cases = [
-    Expect<Equal<LengthOfString<"">, 0>>,
-    Expect<Equal<LengthOfString<"kumiko">, 6>>,
-    Expect<Equal<LengthOfString<"reina">, 5>>,
-    Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
-  ];
+  type res = LengthOfString<"Sound!Euphonium">;
+  // type res = LengthOfString<"kumiko">
+  // type res = LengthOfString<"reina">
+  // type res = LengthOfString<"Sound! Euphonium">
 };
