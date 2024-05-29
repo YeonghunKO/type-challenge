1ad0940cc2b3e5bb3151bbe740f29ad79e589e20
Sinkyo <yhko1988@gmail.com>
2024-05-29 22:08:52 +0900

    feat : done

--- medium/AppendToObj.ts
+++ medium/AppendToObj.ts
@@ -1,9 +1,11 @@
 () => {
+  type Flatten<T> = { [key in keyof T]: T[key] };
+
   type AppendToObject<
-    Obj extends Record<string, any>,
-    key extends string,
-    value
-  > = any;
+    T extends Record<string, any>,
+    U extends string,
+    V
+  > = Flatten<T & { [key in U]: V }>;
 
   type Test = { id: "1" };
   type Result = AppendToObject<Test, "value", 4>; // expected to be { id: '1', value: 4 }
