4f0d87b556cee11f318a5081365a48c99cb25ba9
Sinkyo <yhko1988@gmail.com>
2024-06-12 16:52:26 +0900

    feat : done

--- medium/StringToUnion.ts
+++ medium/StringToUnion.ts
@@ -1,5 +1,8 @@
 (() => {
-  type StringToUnion<T> = any;
+  type StringToUnion<T extends string> =
+    T extends `${infer Letter}${infer Rest}`
+      ? Letter | StringToUnion<Rest>
+      : never;
   type Test = "123";
   type Result = StringToUnion<Test>; // expected to be "1" | "2" | "3"
 })();
