5a54e48a30aae5dd30732b930aeb9802f9b08370
Sinkyo <yhko1988@gmail.com>
2024-06-21 13:58:50 +0900

    feat : done

--- medium/AnyOf.ts
+++ medium/AnyOf.ts
@@ -2,7 +2,18 @@
   /**
    * if anyof the element is truthy, it returns true
    */
-  type AnyOf<T> = any;
-  type Sample1 = AnyOf<[1, "", false, [], {}]>; // expected to be true.
-  type Sample2 = AnyOf<[0, "", false, [], {}]>; // expected to be false.
+
+  type AnyOf<T extends any[]> = T[number] extends
+    | 0
+    | ""
+    | []
+    | false
+    | Record<string, never>
+    | undefined
+    | null
+    ? false
+    : true;
+
+  type Sample1 = AnyOf<[0, "2", false, [], {}, null]>; // expected to be true.
+  type Sample2 = AnyOf<[0, "", false, [], {}, null]>; // expected to be false.
 };
