10a3b9e68b1fed4ecb108e51080862bf4ba62820
budifyio <budify.io@gmail.com>
2024-06-24 19:44:11 +0900

    test

--- medium/IsNever.ts
+++ medium/IsNever.ts
@@ -6,5 +6,5 @@
   type C = IsNever<null>; // expected to be false
   type D = IsNever<[]>; // expected to be false
   type E = IsNever<number>; // expected to be false
-  console.log();
+  console.log("ㅗㅁ모");
 };
